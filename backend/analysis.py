5ec4f92041cd9ecde06e24b40e7f796061f7816e
Vinicius Lepca <vinicius.lepca@gmail.com>
2020-09-12 16:20:18 -0300

    Use tempfile to generate keys

--- backend/analysis.py
+++ backend/analysis.py
@@ -1,14 +1,18 @@
-import os
+import tempfile
+import json
 
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
-# from .keyfile import key_object
+from .keyfile import key_object
 
 # Initialize this project with firebase support
 # KEYS file not shared for security reasons
-cred = credentials.Certificate(os.path.abspath('/tmp/keys.json'))
+f = tempfile.TemporaryFile()
+f.write(bytes(json.dumps(key_object), 'utf-8'))
+cred = credentials.Certificate(f)
+f.close()
 
 # Initialize the app to service account
 firebase_admin.initialize_app(cred, {
